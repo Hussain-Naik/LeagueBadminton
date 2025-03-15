cd9d09e4d3457f1614d5aa91d5cf7c5e7261cf65
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2025-03-15 09:33:30 +0000

    Add routes navigation

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,33 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import 'primeflex/primeflex.css';  
 import 'primereact/resources/primereact.css';
 import 'primeicons/primeicons.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
-import Buttons from "./components/Buttons";
+import { Route, Routes } from 'react-router';
 
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Buttons label="button text" icon="pi pi-check"/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="flex flex-column justify-content-between min-h-screen mx-3">
+          {/* <Header /> */}
+          <main className='col-12 md:col-6 flex-grow-1 m-auto p-0'>
+            <Routes>
+            <Route index element={<h1>Home</h1>} />
+            <Route path="league" element={<h1>League</h1>} />
+            <Route path="session" element={<h1>Session</h1>} />
+            <Route path="session/create" element={<h1>Session Settings</h1>} />
+            <Route path="*" element={<h1>Not found</h1>} />
+            </Routes>
+          </main>
+          {/* <Footer /> */}
+        </div>
   );
 }
 
